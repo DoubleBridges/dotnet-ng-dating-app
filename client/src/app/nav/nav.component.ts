5524dc6d063ae3343e0cf9c40b24b9189f6a30d4
Dave Kane <davelkane@gmail.com>
2021-03-02 21:35:46 -0600

    Added member, lists, and messages components and pages, added routing, added toastr to handle login and register errors

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AccountService } from '../_services/account.service';
+import {Router} from '@angular/router';
+import {ToastrService} from 'ngx-toastr';
 
 @Component({
   selector: 'app-nav',
@@ -9,23 +11,25 @@ import { AccountService } from '../_services/account.service';
 export class NavComponent implements OnInit {
   model: any = {};
 
-  constructor(public accountService: AccountService) {}
+  constructor(public accountService: AccountService, private router: Router, private toastr: ToastrService) {}
 
   ngOnInit(): void {
   }
 
   login() {
     this.accountService.login(this.model).subscribe(
-      (response) => {
-        console.log(response);
+      _ => {
+        this.router.navigateByUrl('/members').then(_ => {});
       },
       (error) => {
         console.log(error);
+        this.toastr.error(error.error);
       }
     );
   }
 
   logout() {
     this.accountService.logout();
+    this.router.navigateByUrl('/').then(() => {});
   }
 }
